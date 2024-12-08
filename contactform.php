34aaa7cce6b4965e5ddea5ec62fb844c1662ed49
AdelinePat <adeline.patenne@hotmail.fr>
2024-12-08 18:15:49 +0100

    working on the contact form, creating contactform.php

---
+++ contactform.php
@@ -0,0 +1,17 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $prenom = $_POST['prenom'];
+    $nom = $_POST['nom'];
+    $mailfrom = $_POST['email'];
+    $telephone = $_POST['telephone'];
+    $objet = $_POST['objet'];
+    $message = $_POST['message'];
+
+    $mailTo = "adeline.patenne@laplateforme.io";
+    $headers = "From: ".$mailFrom
+    $txt = "Vous avez reçu un message de votre portfolio venant de ".$nom.".\n\n".$message; 
+
+    mail($mailTo, $objet, $headers, $txt);
+
+}
