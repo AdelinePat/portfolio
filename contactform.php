ea6771d3cc90c3e690f8c7dd3e9b21fae78dceec
AdelinePat <adeline.patenne@hotmail.fr>
2025-01-04 21:48:53 +0100

    editing portfolio, adding icons + editing footer

--- contactform.php
+++ contactform.php
@@ -8,12 +8,12 @@ if (isset($_POST['submit'])) {
     $objet = $_POST['objet'];
     $message = $_POST['message'];
 
-    $mailTo = "adeline.patenne@laplateforme.io";
-    $headers = "From: ".$mailFrom
-    $txt = "Vous avez reçu un message de votre portfolio venant de ".$nom.".\n\n".$message; 
+    $mailTo = "sachalarcher@gmail.com";
+    $headers = "From: {$mailFrom}";
+    $txt = "Vous avez reçu un message de votre portfolio venant de {$nom} \n\n {$message}"; 
 
     mail($mailTo, $objet, $headers, $txt);
-    header("location: index.php?mailsend")
+    header("location: index.php?mailsend");
 }
 
 ?>
