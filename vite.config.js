adeb9f6583ff4fd32b3a5947b50e95630fd4c7c5
AdelinePat <adeline.patenne@hotmail.fr>
2025-11-30 21:23:01 +0100

    ci: change vite config and deploy-static workflow

--- vite.config.js
+++ vite.config.js
@@ -3,30 +3,54 @@ import { resolve } from "path";
 
 const isWindows = process.platform === "win32";
 
-export default defineConfig({
-  base: "/portfolio/",
-  root: "./src",
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "src/index.html"),
-        contact: resolve(__dirname, "src/contact.html"),
-        projects: resolve(__dirname, "src/projects.html"),
-        certifications: resolve(__dirname, "src/certifications.html"),
+export default defineConfig(() => {
+  const base = process.env.VITE_BASE || "/portfolio/";
+
+  return {
+    base,
+    root: "./src",
+    build: {
+      outDir: "../dist",
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "src/index.html"),
+          contact: resolve(__dirname, "src/contact.html"),
+          projects: resolve(__dirname, "src/projects.html"),
+          certifications: resolve(__dirname, "src/certifications.html"),
+        },
       },
     },
-  },
-  server: {
-    host: "0.0.0.0", // container listens on all interfaces
-    watch: {
-      usePolling: true, // Windows + Docker fix
-      interval: 1000,
-      // followSymlinks: true,
+    server: {
+      host: "0.0.0.0", // container listens on all interfaces
+      watch: {
+        usePolling: true, // Windows + Docker fix
+        interval: 1000,
+      },
     },
-    // hmr: {
-    //   host: "localhost", // browser connects to localhost: mapped by Docker
-    // },
-  },
+  };
 });
+
+// export default defineConfig({
+//   base: "/portfolio/",
+//   root: "./src",
+//   build: {
+//     outDir: "../dist",
+//     emptyOutDir: true,
+//     rollupOptions: {
+//       input: {
+//         main: resolve(__dirname, "src/index.html"),
+//         contact: resolve(__dirname, "src/contact.html"),
+//         projects: resolve(__dirname, "src/projects.html"),
+//         certifications: resolve(__dirname, "src/certifications.html"),
+//       },
+//     },
+//   },
+//   server: {
+//     host: "0.0.0.0", // container listens on all interfaces
+//     watch: {
+//       usePolling: true, // Windows + Docker fix
+//       interval: 1000,
+//     },
+//   },
+// });
